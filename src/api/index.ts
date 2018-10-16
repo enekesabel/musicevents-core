8b533a8b8752193ad4c9586dabf2a080286da19f
Énekes Ábel <enekes.abel@gmail.com>
2018-10-17 01:05:56 +0200

    remote apis exported in api module

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,6 @@
 export * from './dummy_implementation/DummyArtistApi';
 export * from './dummy_implementation/DummyEventApi';
+export * from './dummy_implementation/RemoteArtistApi';
+export * from './dummy_implementation/RemoteEventApi';
 export * from './IEventApi';
 export * from './IArtistApi';
